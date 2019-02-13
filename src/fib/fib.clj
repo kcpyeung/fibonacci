374ecaa916b68c7b885942cd32b6185dd3dd79f8
Smelly Cat <smelly@c.at>
2019-02-13 18:33:24 +1100

    Implement (fib 0)

--- src/fib/fib.clj
+++ src/fib/fib.clj
@@ -3,14 +3,16 @@
 (defn fib [n]
   (loop [prev   0
          next   1
-         result 0
-         i      0]
-    (if (= i n)
-      result
-      (recur next (+ prev next) (+ prev next) (inc i)))))
+         result 1
+         i      1]
+    (cond
+     (= 0 n)   0
+     :else     (if (= i n)
+                 result
+                 (recur next (+ prev next) (+ prev next) (inc i))))))
 
 (defn fib-seq [n]
-  (loop [result []
+  (loop [result [0]
          i      0]
     (if (= i n)
       result
--- test/fib/fib_test.clj
+++ test/fib/fib_test.clj
@@ -3,15 +3,27 @@
               [fib.fib :refer :all]))
 
 (deftest fibonacci
+  (testing "0"
+           (is (= 0 (fib 0))))
+
+  (testing "1"
+           (is (= 1 (fib 1))))
+
   (testing "2"
-           (is (= 2 (fib 2))))
+           (is (= 1 (fib 2))))
+
+  (testing "3"
+           (is (= 2 (fib 3))))
+
+  (testing "4"
+           (is (= 3 (fib 4))))
 
   (testing "10"
-           (is (= 89 (fib 10)))))
+           (is (= 55 (fib 10)))))
 
 (deftest fibonacci-in-a-sequence
   (testing "2"
-           (is (= '(1 2) (fib-seq 2))))
+           (is (= '(0 1 1) (fib-seq 2))))
 
   (testing "10"
-           (is (= '(1 2 3 5 8 13 21 34 55 89) (fib-seq 10)))))
+           (is (= '(0 1 1 2 3 5 8 13 21 34 55) (fib-seq 10)))))
