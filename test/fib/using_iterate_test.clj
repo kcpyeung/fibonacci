c0e7490c673003e2b6132bc55b0e89ad337142ba
Smelly Cat <smelly@c.at>
2019-02-14 09:37:03 +1100

    Implement fib using fib-iterate

--- test/fib/using_iterate_test.clj
+++ test/fib/using_iterate_test.clj
@@ -2,29 +2,29 @@
     (:require [clojure.test :refer :all]
               [fib.using-iterate :refer :all]))
 
-;(deftest fibonacci
-;  (testing "0"
-;           (is (= 0 (fib 0))))
-;
-;  (testing "1"
-;           (is (= 1 (fib 1))))
-;
-;  (testing "2"
-;           (is (= 1 (fib 2))))
-;
-;  (testing "3"
-;           (is (= 2 (fib 3))))
-;
-;  (testing "4"
-;           (is (= 3 (fib 4))))
-;
-;  (testing "10"
-;           (is (= 55 (fib 10))))
-;
-;  (testing "500"
-;           (is
-;             (= 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125
-;                (fib 500)))))
+(deftest fibonacci
+  (testing "0"
+           (is (= 0 (fib 0))))
+
+  (testing "1"
+           (is (= 1 (fib 1))))
+
+  (testing "2"
+           (is (= 1 (fib 2))))
+
+  (testing "3"
+           (is (= 2 (fib 3))))
+
+  (testing "4"
+           (is (= 3 (fib 4))))
+
+  (testing "10"
+           (is (= 55 (fib 10))))
+
+  (testing "500"
+           (is
+             (= 139423224561697880139724382870407283950070256587697307264108962948325571622863290691557658876222521294125
+                (fib 500)))))
 
 (deftest fibonacci-in-a-sequence
   (testing "2"
